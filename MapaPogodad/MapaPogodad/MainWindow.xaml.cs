6b1c8f485850f8357368b6e3c5a62a91fa52260b
Slenzak <104827589+Slenzak@users.noreply.github.com>
2024-02-20 14:47:59 +0100

    Add files via upload

---
+++ MapaPogodad/MapaPogodad/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Maps.MapControl.WPF;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net;
+
+
+namespace MapaPogodad
+{
+    public partial class MainWindow : Window
+    {
+        string cords;
+        public MainWindow()
+        {
+            InitializeComponent();
+            myMap.Focus();
+            myMap.Mode = new AerialMode(true);
+
+        }
+        private void MapWithPushpins_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+            Point mousePosition = e.GetPosition(this);
+            Microsoft.Maps.MapControl.WPF.Location pinLocation = myMap.ViewportPointToLocation(mousePosition);
+            Pushpin pin = new Pushpin();
+            pin.Location = pinLocation;
+            cords = pinLocation.ToString();
+            myMap.Children.Clear();
+            myMap.Children.Add(pin);
+            try
+            {
+                string[] loc = cords.Split(',');
+                string firstloc = loc[0] +","+ loc[1];
+                string secondloc = loc[2] + "," + loc[3];
+                Trace.WriteLine(cords);
+                Trace.WriteLine(secondloc+" "+firstloc);
+                double firstlocdouble = Convert.ToDouble(firstloc);
+                double secondlocdouble = Convert.ToDouble(secondloc);
+                string url = $"https://api.openweathermap.org/data/3.0/onecall?lat={firstlocdouble}&lon={secondlocdouble}&appid=30c4a448b7f41d650026b05f4ddf15b4&units=metric";
+                HttpWebRequest request= (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream resStream = response.GetResponseStream();
+                StreamReader sr = new StreamReader(resStream);
+                Trace.WriteLine(sr.ReadToEnd());
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        
+    }
+}
